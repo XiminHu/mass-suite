e45a58c852ea8b100e15f4bb165d0f5c0e3ec195
XiminHu <xhu66@uw.edu>
2020-04-23 11:19:14 -0700

    tested peaklist workflow, created first execution script

--- mss/mssmain.py
+++ mss/mssmain.py
@@ -15,23 +15,45 @@ import pandas as pd
 
 import peakutils
 from peakutils.plot import plot as pplot
+
 import webbrowser
 import glob
 from pathlib import Path 
+from IPython.display import clear_output
+
+from scipy.interpolate import interp1d
+import scipy
+from scipy import stats
 
 
-def get_scans(path, ms_lv = 1):
+def get_scans(path, ms_all = False, ms_lv = 1):
+    '''
+    The function is used to reorganize the pymzml reading into a list that will have better access
+    
+    path: input mzml path
+    ms_all: if you want all the ms_level be check
+    ms_lv: ms_level you want to export
+    '''
+    
+    #Read path using pymzml
     run = pymzml.run.Reader(path)
+    
     scans = []
-    for scan in run:
-        if scan.ms_level == ms_lv:
+    if ms_all == False:
+        for scan in run:
+            if scan.ms_level == ms_lv:
+                scans.append(scan)
+    elif ms_all == True:
+        for scan in run:
             scans.append(scan)
+            
     return scans
 
 
 #Noise removal
 def noise_removal(mzml_scans, int_thres = 5000):
     '''
+    Remove mz&i pairs that i lower than int_thres from whole mzml file, looping through scans
     the output will overwrite the original mzml file
     '''
     for scan in mzml_scans:
@@ -43,16 +65,21 @@ def noise_removal(mzml_scans, int_thres = 5000):
 
 
 
-def mz_locator(input_list, mz, error):
+def mz_locator(input_list, mz, error, select_app = True): #updated to select_app, when false only select closest one, when true append all, use as a backdoor for now if closest algorithm messed up
     '''
-    Find specific mzs from given mz and error range
+    Find specific mzs from given mz and error range out from a given mz array
     input list: mz list
+    mz: input_mz that want to be found
+    error: error range is now changed to ppm level
     '''
     target_mz = []
     target_index = []
     
-    lower_mz = mz - error
-    higher_mz = mz + error
+    #ppm conversion
+    error = error * 1e-6
+    
+    lower_mz = mz - error * mz
+    higher_mz = mz + error * mz
 
     for i, mzs in enumerate(input_list):
         if mzs < lower_mz:
@@ -61,27 +88,42 @@ def mz_locator(input_list, mz, error):
             if mzs <= higher_mz:
                 target_mz.append(mzs)
                 target_index.append(i)
-        elif mzs > higher_mz:
-                target_mz = 0
-                target_index = 'NA'
-                break
+
+    if select_app == False:
+        if len(target_mz) != 0:
+            target_error = [abs(i - mz) for i in target_mz]
+            minpos = target_error.index(min(target_error)) 
+            t_mz = target_mz[minpos]
+            t_i = target_index[minpos]
+        else:
+            t_mz = 0
+            t_i = 'NA'
+    if select_app == True:
+        t_mz = target_mz
+        t_i = target_index
         
-    return target_mz, target_index
+    return t_mz, t_i
 
 
 #Deal with roi, check peakonly
-def peak_pick(mzml_scans, input_mz, error, peak_base = 5000, thr = 0.02, min_d = 1, rt_window = 1.5, peak_area_thres = 1e5, min_scan = 15, max_scan = 200, max_peak = 7):
+#Deal with roi, check peakonly
+def peak_pick(mzml_scans, input_mz, error, peak_thres = 0.01, thr = 0.02, min_d = 1, rt_window = 1.5, peak_area_thres = 1e5, min_scan = 15, max_scan = 200, max_peak = 5, min_scan_window = 20, sn_range = 7):
     '''
-    rt, ints from ms_chromatogram_list
-    rt_window now set up for minutes
+    firstly get rt, intensity from given mz and error out of the mzml file
+    Then find peak on the intensity array, represent as index --> index
+    Find peak range by looping from peak index forward/backward until hit the peak_base --> l_range,h_range. peakspan = h_range - l_range
+    Trim/correct peak range is too small or too large, using min_scan/max_scan,min_scan_window --> trimed l/h_range
+    Integration of peak based on the given range using simp function --> peakarea
     '''
     
     #Important funciont, may need to be extracted out later
+    #Data output from the chromatogram_plot function
     def ms_chromatogram_list(mzml_scans, input_mz, error):
         '''
         Generate a peak list for specific input_mz over whole rt period from the mzml file
         ***Most useful function!
         '''
+        #Create empty list to store the data
         retention_time = []
         intensity = []
         for scan in mzml_scans:
@@ -89,10 +131,11 @@ def peak_pick(mzml_scans, input_mz, error, peak_base = 5000, thr = 0.02, min_d =
             retention_time.append(scan.scan_time[0])
 
             target_mz, target_index = mz_locator(scan.mz, input_mz, error)
-            if target_index == 'NA':
+            if len(target_index) == 0:
                 intensity.append(0)
             else:
-                intensity.append(sum(scan.i[target_index]))
+                #intensity.append(scan.i[target_index])
+                intensity.append(sum(scan.i[target_index])) #if select_app=True
             
         return retention_time, intensity
     
@@ -106,18 +149,31 @@ def peak_pick(mzml_scans, input_mz, error, peak_base = 5000, thr = 0.02, min_d =
     
     result_dict = {}
     
+    
+    #dev note: boundary detection refinement
     for index in indexes:
         h_range = index
         l_range = index
-        base_intensity = peak_base
+        base_intensity = peak_thres * intensity[index] # use relative thres, also considering S/N, 1/2 rt point?
+        half_intensity = 0.5 * intensity[index]
 
         #Get the higher and lower boundary
         while intensity[h_range] >= base_intensity:
             h_range += 1
-            if h_range > len(intensity)-2:
-                break
-        while intensity[l_range] >= base_intensity:
+            if intensity[h_range] < half_intensity: #potentially record this
+                if h_range - index > 4: #fit r2 score, keep record https://stackoverflow.com/questions/55649356/how-can-i-detect-if-trend-is-increasing-or-decreasing-in-time-series as alternative
+                    x = np.linspace(h_range - 2, h_range, 3)
+                    y = intensity[h_range - 2 : h_range + 1]
+                    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
+#                     print(rt[h_range],r_value)
+                    if abs(r_value) < 0.6:
+                        break
+                    elif h_range > len(intensity)-2: 
+                        break
+        while intensity[l_range] >= base_intensity: #Dev part 2, low priority since general peak shapes
             l_range -= 1
+            if intensity[l_range] < half_intensity:
+                pass #backdoor for recording 1/2 rt point
         #Output a range from the peak list
         
         peak_range = []
@@ -129,17 +185,35 @@ def peak_pick(mzml_scans, input_mz, error, peak_base = 5000, thr = 0.02, min_d =
                 h_range = index + scan_window
                 peak_range = intensity[l_range:h_range]
                 #print(index + scan_window)
+                
+        #Calculate for S/N
+        signal = intensity[index]
+        neighbour_blank = intensity[l_range - sn_range : l_range] + intensity[h_range + 1 : h_range + sn_range + 1]
+        noise = max(neighbour_blank)
+        sn = round(signal/noise, 3)
+        
+        #Calculate height/width, consider log10 transform
+        height = signal
+        width = rt[h_range] - rt[l_range]
+        hw_ratio = round(height/width,0)
+        
 
         #Intergration based on the simps function
         if len(peak_range) >= min_scan:
             integration_result = simps(peak_range)
             if integration_result >= peak_area_thres:
+                #Calculate Area/background ratio, i.e, peak area vs rectangular area as whole(if =1 then peak is a pleateu)
+                background_area = (h_range - l_range) * height
+                ab_ratio = round(integration_result/background_area, 3)
+                
+                #appending to result
                 if len(result_dict) == 0:
-                    result_dict.update({index : [l_range, h_range, integration_result]})
+                    result_dict.update({index : [l_range, h_range, integration_result, sn, hw_ratio, ab_ratio]})
                 elif integration_result != list(result_dict.values())[-1][2]: #Compare with previous item
                     s_window = abs(index - list(result_dict.keys())[-1])
                     if s_window > min_scan_window:
-                        result_dict.update({index : [l_range, h_range, integration_result]})
+                        result_dict.update({index : [l_range, h_range, integration_result, sn, hw_ratio, ab_ratio]})
+                    
                 
         #Filtering:
         #1. delete results that l_range/h_range within 5 scans
@@ -157,33 +231,45 @@ def peak_pick(mzml_scans, input_mz, error, peak_base = 5000, thr = 0.02, min_d =
 
 #Main dev issue--iteration efficiency & peak picking algorithm update
 #2way: 1. optimize iteration algorithm, 2. optimize mz_list selection--drop useless mzs
-def peak_list(mzml_scans, mz_error, mzmin = 0, mzmax=1000, peak_base = 0.005, thr = 0.02, min_d = 1, rt_window = 1.5, peak_area_thres = 1e5, min_scan = 7, scan_thres = 7):
+#Main dev issue--iteration efficiency & peak picking algorithm update
+#2way: 1. optimize iteration algorithm, 2. optimize mz_list selection--drop useless mzs
+def peak_list(mzml_scans, err_ppm = 20, mz_c_thres = 5, peak_base = 0.005, thr = 0.02, min_d = 1, rt_window = 1.5, peak_area_thres = 1e5, min_scan = 7, scan_thres = 7):
     '''
-    input from ms1_baseline function
+    Generate a dataframe by looping throughout the whole mz space from a given mzml file
+    ref to peak_picking function
     Q to solve: how to correctly select mz slice?? see mz_locator
     '''
     
-    #Get m/z range
-    def mz_array(scans, mz_error):
-        min_mz = scans[0].mz.min()
-        max_mz = scans[0].mz.max()
-        for scan in scans:
-            if min_mz > scan.mz.min():
-                min_mz = scan.mz.min()
-            if max_mz < scan.mz.max():
-                max_mz = scan.mz.max()
+    #Get m/z range -- updated 0416
+    print('Generating mz list...')
+    def mz_gen(mzml_scans, err_ppm, mz_c_thres):
+        pmz = []
+        for scan in mzml_scans:
+            pmz.append(scan.mz)
+        pmz = np.hstack(pmz).squeeze()
 
-        mz_list = np.arange(min_mz + mz_error, max_mz - mz_error,2 * mz_error)
+        def mz_list_gen(minmz, maxmz, error_ppm):
+            error = error_ppm * 1e-6
+            mz_list = [minmz]
+            mz = minmz
+            while mz <= maxmz:
+                mz = mz + 2 * error * mz
+                mz_list.append(mz)
+            return mz_list
 
-        return mz_list
-    
-    mzlist = mz_array(mzml_scans, mz_error)
-    
-    if mzlist.max() >= mzmax:
-        mzlist = mzlist[mzlist <= mzmax]
-    if mzlist.min() <= mzmin:
-        mzlist = mzlist[mzlist >= mzmin]
-    
+        mz_list = mz_list_gen(pmz.min(), pmz.max(), err_ppm)
+
+        final_mz = []
+        for m in mz_list:
+            lm = m - err_ppm * 1e-6 * m
+            hm = m + err_ppm * 1e-6 * m
+            if len(pmz[(pmz <= hm) & (pmz >= lm)]) >= mz_c_thres:
+                final_mz.append(m)
+
+        return final_mz
+        
+    mzlist = mz_gen(mzml_scans, err_ppm, mz_c_thres) #New list for looping
+    print('Finding peaks...')
     
     result_dict = {}
     rt = []
@@ -192,23 +278,26 @@ def peak_list(mzml_scans, mz_error, mzmin = 0, mzmax=1000, peak_base = 0.005, th
     
     for mz in tqdm(mzlist):
         try:
-            peak_dict = peak_pick(mzml_scans, mz, mz_error)
+            peak_dict = peak_pick(mzml_scans, mz, err_ppm)
         except:
             pass
         
-        if len(result_dict) == 0:
-            for index in peak_dict:
-                result_dict.update({'m/z' : [mz],
-                                    'rt' : [rt[index]],
-                                    'peak area' : [peak_dict[index][2]]})
-        else:
-            for index in peak_dict:
-                result_dict['m/z'].append(mz)
-                result_dict['rt'].append(rt[index])
-                result_dict['peak area'].append(peak_dict[index][2])
-            
+        if len(peak_dict) != 0:
+            if len(result_dict) == 0:
+                for index in peak_dict:
+                    result_dict.update({'m/z' : [mz],
+                                        'rt' : [rt[index]],
+                                        'peak area' : [peak_dict[index][2]]})
+            else:
+                for index in peak_dict:
+                    result_dict['m/z'].append(mz)
+                    result_dict['rt'].append(rt[index])
+                    result_dict['peak area'].append(peak_dict[index][2])
+    #print(result_dict)        
     print('Peak processing finished!')
     d_result = pd.DataFrame(result_dict)
+    d_result['rt'] = round(d_result['rt'], 2)
+    d_result['m/z'] = round(d_result['m/z'], 4)
     print('Dataframe created!')
     
     return d_result
