a5066f060cc2194444cecd7315799f1e39e35a90
Derek Mar <dmar19@uw.edu>
2020-05-08 17:12:02 -0700

    added line to help resolve NaN

--- dev/mss_processor.py
+++ dev/mss_processor.py
@@ -88,6 +88,10 @@ def alignment(d_batch, export_name, rt_error, MZ_error):
     print("Initial reference built")
     print("Alignment beginning..")
     for row in tqdm(range(len(total_samp))):
+        if alignment_df.isnull().values.any() is True:
+            alignment_df = alignment_df.fillna(0)
+        else:
+            pass
         row_max = len(alignment_df)
         if row < range(len(total_samp))[-1]:
             # Loops until a change in sample occurs by index
