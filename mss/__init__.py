32290ae64c67136740d750a986f9434feddf21e5
Derek Mar <dmar19@uw.edu>
2020-06-18 16:13:12 -0700

    updated to resolve errors

--- mss/__init__.py
+++ mss/__init__.py
@@ -1,6 +1,9 @@
 from mss import visreader
 from mss import mssmain
 from mss import alignment
+from mss import cluster
+from mss import frag
+from mss import mssdata
 from .version import __version__
 
 __all__ = [visreader, mssmain, alignment, cluster, frag, mssdata, __version__]
