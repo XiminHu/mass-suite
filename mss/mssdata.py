9899c3b18d90be56f32119c0e6ba97f4bad8629f
Nozomi Suzuki <diegocat@uw.edu>
2020-06-18 19:20:28 -0700

    pep8

--- mss/mssdata.py
+++ mss/mssdata.py
@@ -1,16 +1,15 @@
-#import tensorflow as tf 
+# import tensorflow as tf
 import os
 import pickle
-import sklearn
 
 
 this_dir, this_filename = os.path.split(__file__)
 
-class peakmodel:
-	#test_model_path = os.path.join(this_dir, 'model_5_3_14_4_48.h5')
-	#test_model = tf.keras.models.load_model(test_model_path)
-	Model_file = os.path.join(this_dir, 'rfmodel.pkl')
-	rf_model = pickle.load(open(Model_file, 'rb'))
-	Model_file_t = os.path.join(this_dir, 'rfmodel_tuned.pkl')
-	rf_model_t = pickle.load(open(Model_file_t, 'rb'))
 
+class peakmodel:
+    # test_model_path = os.path.join(this_dir, 'model_5_3_14_4_48.h5')
+    # test_model = tf.keras.models.load_model(test_model_path)
+    Model_file = os.path.join(this_dir, 'rfmodel.pkl')
+    rf_model = pickle.load(open(Model_file, 'rb'))
+    Model_file_t = os.path.join(this_dir, 'rfmodel_tuned.pkl')
+    rf_model_t = pickle.load(open(Model_file_t, 'rb'))
