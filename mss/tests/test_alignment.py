d679ffe970f43370e1e0e341184390bd4ae4c22b
Derek Mar <dmar19@uw.edu>
2020-06-18 19:25:45 -0700

    resolved pep8 errors

--- mss/tests/test_alignment.py
+++ mss/tests/test_alignment.py
@@ -1,9 +1,8 @@
 import os
 import unittest
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
-from tqdm import tqdm
 
 import mss
 from mss import alignment
@@ -11,23 +10,20 @@ from mss import alignment
 data_path = os.path.dirname(os.path.join(mss.__path__[0]))
 batch_path = data_path + '/example_data/batch/'
 
+
 class test_alignment(unittest.TestCase):
-    
     def test_stack(self):
         """Tests stack functionlity"""
-        
         data_path = os.path.dirname(os.path.join(mss.__path__[0]))
         batchpath = data_path + '/example_data/batch/'
 
         a = alignment.stack(batchpath)
-        
         assert len(a) == 2, "output size is wrong"
         assert type(a) == tuple, "output type is wrong"
-        assert type(a[1].iat[6,2]) == np.float32, "stacking seems off"
-        
+        assert type(a[1].iat[6, 2]) == np.float32, "stacking seems off"
+
     def test_realignment(self):
         """Tests realignment functionality"""
-        
         data_path = os.path.dirname(os.path.join(mss.__path__[0]))
         batchpath = data_path + '/example_data/batch/'
 
@@ -35,10 +31,8 @@ class test_alignment(unittest.TestCase):
         rt_error = 0.05
         MZ_error = 0.015
         file_type = 'csv'
-        
-        b = alignment.realignment(batchpath, batch_name, file_type, rt_error, MZ_error)
-        
+        b = alignment.realignment(batchpath, batch_name, file_type,
+                                  rt_error, MZ_error)
         assert len(b) == 1486, "testing realingment length is off"
         assert type(b) == pd.core.frame.DataFrame, "output type is wrong"
-        assert type(b.iat[1,2]) == np.float32, "realignment seems to be going incorrectly"
-
+        assert type(b.iat[1, 2]) == np.float32, "realignment seems to be off"
