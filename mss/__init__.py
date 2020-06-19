968fa8d7cc0946319422b95a064c9e16281c7a28
XiminHu <xhu66@uw.edu>
2020-06-19 14:30:24 -0700

    fixed a import error

--- mss/__init__.py
+++ mss/__init__.py
@@ -1,11 +1,10 @@
 from mss import visreader
 from mss import mssmain
 from mss import alignment
-from mss import cluster
 from mss import frag
 from mss import mssdata
 from mss import dm
 from .version import __version__
 
 __all__ = [visreader, mssmain, alignment,
-           cluster, frag, mssdata, dm, __version__]
+           frag, mssdata, dm, __version__]
