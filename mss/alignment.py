ec70e90d8f994b97e529f6b51f8d9c324a5495dd
Derek Mar <dmar19@uw.edu>
2020-06-19 19:40:32 -0700

    test

--- mss/alignment.py
+++ mss/alignment.py
@@ -17,9 +17,7 @@ def stack(batch_path):
     # Reading in files into a list
     for files in range(num_files):
         sample = os.listdir(batch_path)[files]
-        sample_df = pd.read_csv(batch_path + sample, usecols=['m/z',
-                                'rt', 'sn', 'score', 'peak area'],
-                                dtype=np.float32)
+        sample_df = pd.read_csv(batch_path + sample, dtype=np.float32)
         all_samples += [sample_df]
     # Combining all the dataframes into one
     total_samples = pd.concat(all_samples)
