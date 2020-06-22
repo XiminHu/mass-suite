d5859a78e842c67888e21649d24a5f92abc8a42d
XiminHu <xhu66@uw.edu>
2020-06-22 13:25:36 -0700

    visreader test update

---
+++ mss/tests/test_visreader.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+import re
+from scipy.integrate import simps
+import peakutils
+import webbrowser
+import pyperclip
+import mss
+from mss import visreader
+
+
+class test_dm(unittest.TestCase):
+    def test_mz_locator(self):
+        test_array = [0, 10, 20, 50, 100]
+        result = visreader.mz_locator(test_array, 49.9999, 20)
+        assert result[0][0] == 50, 'Wrong mz found'
+        assert result[1][0] == 3, 'Wrong index found'
+        return
+
+    def test_formula_mass(self):
+        test_formula = 'C18H22N2O2'
+        result = visreader.formula_mass(test_formula)
+        assert result == 299.1764085799, 'Wrong output'
+        return
