da79f42c1c0fa2aaac6018c953430f0b99ecd290
XiminHu <xhu66@uw.edu>
2021-01-19 17:57:43 -0800

    code style check

--- mss/tests/test_alignment.py
+++ mss/tests/test_alignment.py
@@ -14,13 +14,12 @@ class test_alignment(unittest.TestCase):
     def test_stack(self):
         """Tests stack functionlity"""
         test = align.mss_process(batchpath, None, thres_noise=20000,
-        	                     enable_score=False)
+                                 enable_score=False)
         assert len(test) > 0, "no df read in the folder"
 #        assert len(b) > 0, "output size is wrong"
 #        assert type(b) == pd.core.frame.DataFrame, "output type is wrong"
 #        assert b[b.columns[0]].dtypes == np.float32, "stacking seems off"
 #        assert len(b.columns) > 0, 'no columns'
-
 #    def test_realignment(self):
 #        """Tests realignment functionality"""
 #        batch_name = 'test1'
@@ -28,7 +27,7 @@ class test_alignment(unittest.TestCase):
 #        MZ_error = 0.015
 #        file_type = '.csv'
 #        b = align.mss_align(batchpath, batch_name, file_type,
-#                                  rt_error, MZ_error)
+#                            rt_error, MZ_error)
 #        assert len(b) > 0, "testing realingment length is off"
 #        assert type(b) == pd.core.frame.DataFrame, "output type is wrong"
 #        assert b[b.columns[0]].dtypes == np.float32, "wrong value type"
