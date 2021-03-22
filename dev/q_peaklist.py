cf06321979dc92f579a40c3c145b181ea5969827
XiminHu <xhu66@uw.edu>
2021-03-22 14:14:05 -0700

    updates on the benchmark

--- dev/q_peaklist.py
+++ dev/q_peaklist.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../')
 from mss import mssmain as msm
 from mss import align
-
+'''
 path = input('filename to process:')
 scans = msm.get_scans(path, ms_all=False, ms_lv=1)
 #noise removal
@@ -11,3 +11,7 @@ msm.noise_removal(scans, 2000)
 d_op = msm.peak_list(scans, 10, enable_score=True,peak_base=0.001,peak_area_thres=0)
 output_path = input('path and filename to export:')
 d_op.to_csv(output_path)
+'''
+data_path = input('data path:')
+output_path = input('output path:')
+align.mss_process(data_path, output_path, thres_noise=2000,enable_score=False)
